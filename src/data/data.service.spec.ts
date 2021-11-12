372e199c89c69a6a68778feed0de3a67f8f3e7b3
Styn Vercauteren <styn.vercauteren@outlook.com>
2021-11-12 15:28:48 +0100

    add service unit test

--- src/data/data.service.spec.ts
+++ src/data/data.service.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataService } from './data.service';
 import { DataModule } from './data.module';
+import { Data } from './interfaces/data.interface';
 import { getModelToken } from '@nestjs/mongoose';
 
 describe('DataService', () => {
   let dataService: DataService;
 
-  const mockRepository = {
-    findByType() {
-      return {};
+  const testData = {
+    type: 'type',
+    title: 'title',
+    subtitle: 'subtitle',
+    body: 'body',
+    icons: ['icons'],
+    duration: 'duration',
+    received: 'received',
+  };
+
+  const mockDataModel = {
+    findByType(type: string) {
+      return [testData];
     },
   };
 
@@ -17,7 +28,7 @@ describe('DataService', () => {
       imports: [DataModule],
     })
       .overrideProvider(getModelToken('Data'))
-      .useValue(mockRepository)
+      .useValue(mockDataModel)
       .compile();
 
     dataService = module.get<DataService>(DataService);
@@ -25,5 +36,10 @@ describe('DataService', () => {
 
   it('should be defined', () => {
     expect(dataService).toBeDefined();
+  });
+
+  it('findByType should return a value', () => {
+    dataService.findByType('test');
+    expect(mockDataModel.findByType('test')).toStrictEqual([testData]);
   });
 });
