8c2f70f8ead54a1f2c2ac0443dd759f76cd0d5ed
Styn Vercauteren <styn.vercauteren@outlook.com>
2021-11-12 12:30:41 +0100

    add heroku logging

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, function () {
+    console.log('Express server listening on port %d', this.address().port);
+  });
 }
 bootstrap();
